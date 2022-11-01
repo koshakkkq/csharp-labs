896245a5cd8daa84eb0565aa2067273468407944
koshak <kokolen77@gmail.com>
2022-11-01 11:09:31 +0300

    lab4

--- Rybakov/ProductDetails.aspx.cs
+++ Rybakov/ProductDetails.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Rybakov.Models;
+using System.Web.ModelBinding;
+using Rybakov.Models;
 
 namespace Rybakov
 {
@@ -12,6 +15,21 @@ namespace Rybakov
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetProduct([QueryString("productID")] int? productId)
+        {
+            var _db = new Rybakov.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if (productId.HasValue && productId > 0)
+            {
+                query = query.Where(p => p.ProductID == productId);
+            }
+            else
+            {
+                query = null;
+            }
+            return query;
         }
     }
 }
